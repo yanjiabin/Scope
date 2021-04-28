7a4149370207c2f5cda1d362e1920f185d047cb0
zxs_android_1 <285112784@qq.com>
2021-04-28 14:58:42 +0800

    1.实现音乐播放加载动画控件效果

---
+++ loadingview/src/main/java/com/asa/loadingview/LoadingView.kt
@@ -0,0 +1,238 @@
+package com.asa.loadingview
+
+import android.animation.AnimatorSet
+import android.animation.ValueAnimator
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.util.Log
+import android.util.SparseArray
+import android.view.View
+import androidx.core.content.ContextCompat
+import kotlin.math.max
+
+class LoadingView:View {
+
+    companion object{
+        val PLAY_STATE_PLAYING = 1
+        val PLAY_STATE_WAIT = 2
+    }
+
+    private val paint = Paint()
+    private var bgColor = 0
+    private var columnarTopColor = 0
+    private var columnarBottomColor = 0
+    private var diameter = 0
+
+    private var dotRadius = 0
+    private var dotType = 0
+    private var dotCount = 0
+
+    private var columnarSpan = 5
+    private var columnarWidth = 0
+    private var columnarDefaultHeight = 40f
+    private var columnarTop = 0f
+    private var columnarBottom = 0f
+    private val rect = RectF()
+
+
+    private var columnar1AnimTop = columnarDefaultHeight
+    private var columnar2AnimTop = columnarDefaultHeight
+    private var columnar3AnimTop = columnarDefaultHeight
+    private val columnarAnimSet = AnimatorSet()
+    private val dotAnim = ValueAnimator.ofInt(0,3)
+
+    var duration = 800L
+    var play_state = PLAY_STATE_PLAYING
+
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ){
+        bgColor = ContextCompat.getColor(context, R.color.color_90B2FF)
+        columnarTopColor = ContextCompat.getColor(context, R.color.white)
+        columnarBottomColor = ContextCompat.getColor(context, R.color.color_D3E0FF)
+        columnarWidth = context.resources.getDimensionPixelOffset(R.dimen.dp_10)
+        dotRadius = context.resources.getDimensionPixelOffset(R.dimen.dp_3)
+        dotRadius = context.resources.getDimensionPixelOffset(R.dimen.dp_3)
+        columnarSpan = context.resources.getDimensionPixelOffset(R.dimen.dp_3)
+
+        paint.isAntiAlias = true
+        paint.isDither = true
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val width = MeasureSpec.getSize(widthMeasureSpec)
+        val height = MeasureSpec.getSize(heightMeasureSpec)
+        diameter = max(width, height)
+        setMeasuredDimension(diameter, diameter)
+        columnarTop = (diameter * 0.24f)
+        columnarBottom = (diameter * 0.76f)
+
+        initColumnarAnim()
+        initDotAnim()
+    }
+
+    private fun initColumnarAnim(){
+        val minHeight = columnarBottom * 0.8f
+        val columnar1Anim = ValueAnimator.ofFloat(minHeight,columnarTop)
+        columnar1Anim.repeatMode = ValueAnimator.REVERSE
+        columnar1Anim.repeatCount = ValueAnimator.INFINITE
+
+        val columnar2Anim = columnar1Anim.clone()
+        val columnar3Anim = columnar1Anim.clone()
+
+        columnar3Anim.addUpdateListener {
+            columnar1AnimTop = columnar1Anim.animatedValue as Float
+            columnar2AnimTop = columnar2Anim.animatedValue as Float
+            columnar3AnimTop = columnar3Anim.animatedValue as Float
+            invalidate()
+        }
+        columnar2Anim.startDelay = duration / 2
+        columnar3Anim.startDelay = duration
+
+        columnarAnimSet.playTogether(columnar1Anim,columnar2Anim,columnar3Anim)
+        columnarAnimSet.duration = duration
+        columnarAnimSet.start()
+    }
+
+    private fun initDotAnim(){
+        dotAnim.repeatMode = ValueAnimator.RESTART
+        dotAnim.repeatCount = ValueAnimator.INFINITE
+        dotAnim.addUpdateListener {
+//            dotType = it.animatedValue as Int
+            dotCount = it.animatedValue as Int
+            invalidate()
+        }
+        dotAnim.duration = duration + 1000
+        dotAnim.start()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        drawBg(canvas)
+        if (play_state == PLAY_STATE_PLAYING){
+            drawColumnar(canvas)
+        }else if (play_state == PLAY_STATE_WAIT){
+            drawDot(canvas)
+        }
+    }
+
+    private fun drawDot(canvas: Canvas) {
+        paint.color = columnarTopColor
+        val dotDiameter = dotRadius * 2
+        var left = diameter / 2 - dotDiameter/2 - dotDiameter - columnarSpan
+        var top = height / 2 - dotDiameter/2
+        var right = left + dotDiameter
+        val bottom = top + dotDiameter
+        for(i in 0..dotCount){
+//            left = getLeft(dotType,i)
+//            right = left + dotDiameter
+            rect.set(left.toFloat(), top.toFloat(), right.toFloat(), bottom.toFloat())
+            canvas.drawRoundRect(rect, 4f, 4f, paint)
+            left += (dotDiameter + columnarSpan)
+            right = left + dotDiameter
+        }
+    }
+
+    private fun getLeft(dotType: Int, index: Int):Int{
+        val dotDiameter = dotRadius * 2
+        var left = diameter / 2 - dotDiameter/2 - dotDiameter - columnarSpan
+        when(dotType){
+            0->{
+                return if (index == 0){
+                    left
+                } else {
+                    (left + (dotDiameter + columnarSpan))
+                }
+            }
+            1 -> {
+                return if (index == 0){
+                    (left + (dotDiameter + columnarSpan))
+                }else{
+                    left + ((dotDiameter + columnarSpan) * 2)
+                }
+            }
+            2 -> {
+                return if (index == 0){
+                    left + ((dotDiameter + columnarSpan) * 2)
+                }else{
+                    left
+                }
+            }
+        }
+        return 0
+    }
+
+    private fun drawColumnar(canvas: Canvas) {
+        paint.color = columnarTopColor
+        var left = diameter / 2 - columnarWidth/2 - columnarWidth - columnarSpan
+        var top = columnarTop / 2f
+        var right = left + columnarWidth
+        val bottom = columnarBottom
+        for (i in 1..3){
+            top = getTop(i)
+            rect.set(left.toFloat(), top, right.toFloat(), bottom.toFloat())
+            val shader = getLinearGradient(top)
+            paint.shader = shader
+            canvas.drawRoundRect(rect, 4f, 4f, paint)
+            left += (columnarWidth + columnarSpan)
+            right = left + columnarWidth
+        }
+    }
+
+    private val shaderList = SparseArray<LinearGradient>()
+
+    private fun getLinearGradient(top:Float):LinearGradient{
+        var shader = shaderList.get(top.toInt())
+        if (shader == null){
+            shader = LinearGradient(0f, top, 0f, bottom.toFloat(), columnarTopColor,columnarBottomColor, Shader.TileMode.CLAMP)
+            shaderList.put(top.toInt(),shader)
+        }
+        return shader
+    }
+
+    private fun getTop(index: Int): Float {
+        return when(index) {
+            1-> columnar1AnimTop
+            2-> columnar2AnimTop
+            3-> columnar3AnimTop
+            else -> columnarDefaultHeight
+        }
+    }
+
+
+
+    private fun drawBg(canvas: Canvas) {
+        paint.color = bgColor
+        paint.shader = null
+        canvas.drawCircle(diameter / 2f, diameter / 2f, diameter / 2f, paint)
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        if (play_state == PLAY_STATE_PLAYING){
+            dotAnim.pause()
+            columnarAnimSet.resume()
+            columnarAnimSet.start()
+        }else{
+            columnarAnimSet.pause()
+            dotAnim.resume()
+            dotAnim.start()
+        }
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        dotAnim.pause()
+        columnarAnimSet.pause()
+    }
+
+
+
+
+}
